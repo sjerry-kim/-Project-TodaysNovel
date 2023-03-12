5028e46ddb8fc0a9d8ac63161ea293b359871df3
jinhye <sjrry112@gmail.com>
2023-03-12 18:33:55 +0900

    230312_1st_햄버거바_클릭영역확대_수정_예정

--- project-hp/src/components/NavbarLists.jsx
+++ project-hp/src/components/NavbarLists.jsx
@@ -1,27 +1,65 @@
+import { useEffect, useRef } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import '../css/NavbarLists.css';
 
 const NavbarLists = () => {
+  const [bodyOverflow, setBodyOverflow] = useState(false);
+  const navRef = useRef(null);
+  
+  useEffect(()=>{
+    if(bodyOverflow){
+      document.querySelector("body").style.overflow = "hidden"
+    }else{
+      document.querySelector("body").style.overflow = "auto"
+    }
+  },[bodyOverflow])
+
+  useEffect(()=>{
+    const handler = (event) => {
+      if(navRef.current && !navRef.current.contains(event.target)){
+        setBodyOverflow(false)
+        document.querySelector("#NavbarLists-hamburger").checked = false
+      }
+    };
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  })
+
+
   return (  
     <div className="NavbarLists-wp">
       <input type="checkbox" id="NavbarLists-hamburger" />
       <label class="NavbarLists-menuicon" for="NavbarLists-hamburger">
         <span onClick={()=>{
-          // sessionStorage.setItem("navbar", true);
-          // if(sessionStorage.getItem("navbar") == true){
-          //   sessionStorage.setItem("navbar", !sessionStorage.getItem("navbar"))
-          // }
+          setBodyOverflow(!bodyOverflow);
+          console.log(bodyOverflow);
         }}
+        ref={navRef}
         ></span>
       </label>
-      <div className="NavbarLists-menu">
+      <div className="NavbarLists-menu" >
         <NavLink to="/main">Main</NavLink>
         <NavLink to="/about">About this site</NavLink>
       </div>
-      <div className={`NavbarLists-transparents ${sessionStorage.getItem("navbar") ? ("display-none"):("display-block")}` }
-        onClick={()=>{sessionStorage.setItem("navbar",false)}}
-        ></div>
+      {/* <div className="NavbarLists-transparents"
+        onClick={()=>{
+          const handler = (event) => {
+            if(navRef.current && !navRef.current.contains(event.target)){
+              setBodyOverflow(false)
+              document.querySelector("#NavbarLists-hamburger").checked = false
+            }
+          };
+          document.addEventListener("mousedown", handler);
+      
+          return () => {
+            document.removeEventListener("mousedown", handler);
+          };
+        }}></div> */}
     </div>
   );
 }
